076cf6e9d9107b9709ded03737f4e30a2b1ea1c9
Lethabo008 <lmokoena008@gmail.com>
2026-05-22 19:04:27 -0400

    u

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
-
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
